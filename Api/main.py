10bc6639804d7d283f4eb9e503951719de56f36f
YosefGu <yos770yos@gmail.com>
2025-07-22 17:20:20 +0300

    Api for prediction

---
+++ Api/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from manager import Manager
+
+manager = Manager()
+app = FastAPI()
+
+class InputData(BaseModel):
+    Browser: str
+    Device_Type: str
+    Is_Student: str
+    Visited_Site: str
+    Usage_Frequency: str
+    Income_Level: str
+
+@app.get("/")
+def root():
+    return {"status": "Server is running!"}
+
+@app.post("/predict")
+def predict(data: InputData):
+    sample = {
+        "Browser": data.Browser,
+        "Device_Type": data.Device_Type,
+        "Is_Student": data.Is_Student,
+        "Visited_Site": data.Visited_Site,
+        "Usage_Frequency": data.Usage_Frequency,
+        "Income_Level": data.Income_Level
+    }
+    
+    prediction = manager.predict(sample)
+    return {"prediction": prediction}
+
